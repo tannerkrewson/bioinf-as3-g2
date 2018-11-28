43cb111e1df9409b6668d9896bd4c285c8486f21
Domenic Cusanelli <stl3339@gmail.com>
2018-11-27 19:59:05 -0600

    add multi_seq_alignment function

--- alignment.py
+++ alignment.py
@@ -1,24 +1,26 @@
 def pairwise_alignment():
     return False
 
-def multi_seq_alignment)():
-    return False
+def multi_seq_alignment)( sequences, guide_tree):
+    aligned_sequences = progressive_alignment( sequences, guide_tree )
+    ordered_sequences = reorder_alignments(aligned_sequences[0])
+    return ordered_sequences
 
 def progressive_alignment( sequences, guide_tree, alignments ):
 
     #recursive calls if a tree member is a tuple (not a leaf)
     if type(guide_tree[0]) == tuple:
-        seq_1 = progressive_alignment( sequences, guide_tree[0], alignments )
+        seq_1 = progressive_alignment( sequences, guide_tree[0], alignments )[0]
     else:
         seq_1 = [[sequences[guide_tree[0]][1], guide_tree[0]]]
 
     if type(guide_tree[1]) == tuple:
-        seq_2 = progressive_alignment( sequences, guide_tree[1], alignments )
+        seq_2 = progressive_alignment( sequences, guide_tree[1], alignments )[0]
     else:
         seq_2 = [[sequences[guide_tree[1]][1], guide_tree[1]]]
     #if the tree is a tuple (a leaf), alignment is called on the sequences
     
-    return align_alignments(seq_1, seq_2)[0]
+    return align_alignments(seq_1, seq_2)
 
 def align_alignments( alignment_1, alignment_2 ):
     #core code written by Kimberlyn, repurposed by Domenic
