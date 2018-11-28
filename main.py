2211524a28fb4a17b61baeb44da4a5f78fba1684
tannerkrewson <tannerkrewson@gmail.com>
2018-11-27 19:57:32 -0600

    Get sequences from file via cmd line args

--- main.py
+++ main.py
@@ -1,17 +1,49 @@
 from readfasta import readfasta
-import glob, os
+import glob, os, sys
+
+# from output import align
 
 def main():
     print( "*****\nBioinformatics - Assignment 3 - Group 2\n*****\n" )
 
+    sequences = {}
+
     # scan in all fasta files in the "genes" directory
     os.chdir( os.getcwd() + "/genes/" )
-    file = glob.glob( "*.fasta" )[0]
+    for file in glob.glob( "*.fasta" ):
+        # read all the genes from the fasta file
+        print( file )
+        filename = file.split(".")[0]
 
-    # read all the genes from the fasta file
-    print( file )
-    genes = readfasta( file )
-    print( genes )
+        genes = readfasta( file )
+        sequences[filename] = genes
+
+    # grab the selected sequences from the cmd line args
+    sequences_to_align = get_sequences_to_align_from_command_line( sequences )
+
+    for seq in sequences_to_align:
+        print(seq[0])
+
+
+def get_sequences_to_align_from_command_line( all_sequences ):
+    # eg: main.py H1N1 0 1 4 H5N1 0 1
+    # would align the first, second, and fifth sequences from the H1N1 file
+    # and the first and second sequences from the H5N1 file all in 
+    # multisequence alignment
+
+    sequences_to_align = []
+    current_file = ""
+
+    # for each cmd line argument (0 is "main.py", so skip that)
+    for i in range(1, len(sys.argv)):
+        arg = sys.argv[i]
+        if arg.startswith("H"):
+            current_file = arg
+        else:
+            the_sequence = all_sequences[current_file][int(arg)]
+            sequences_to_align.append(the_sequence)
+
+    return sequences_to_align
 
 main()
 
