6795285eea0fcbd48b6eb0cdb0d770e92521fce7
tannerkrewson <tannerkrewson@gmail.com>
2018-11-29 22:32:59 -0600

    use alignment object

--- visualize.py
+++ visualize.py
@@ -1,19 +1,25 @@
-def alignment_output( sequence1, sequence2 ):
+def alignment_output( alignment ):
+    seq_1 = alignment.alignment_list[0]
+    seq_2 = alignment.alignment_list[1]
+
+    seq_1_name = alignment.name_list[0]
+    seq_2_name = alignment.name_list[1]
+
     IDENTIFIER_LENGTH = 16
     LINE_LENGTH = 50
     SEQ_POSITION_LENGTH = 4
 
-    longer_sequence = max(len(sequence1[1]), len(sequence2[1]))
-    sequence1_pos = 0
-    sequence2_pos = 0
+    longer_sequence = max(len(seq_1), len(seq_2))
+    seq_1_pos = 0
+    seq_2_pos = 0
     pad_string = " " * (IDENTIFIER_LENGTH + SEQ_POSITION_LENGTH)
 
     while longer_sequence > 0:
-        print(f"{sequence1[0]:IDENTIFIER_LENGTH}{sequence1_pos:SEQ_POSITION_LENGTH}{sequence1[1]:LINE_LENGTH} ")
-        print(pad_string, middle_match_line(sequence1[:LINE_LENGTH], sequence2[:LINE_LENGTH], sequence1_pos, sequence2_pos))
-        print(f"{sequence2[0]:IDENTIFIER_LENGTH}{sequence2_pos:SEQ_POSITION_LENGTH}{sequence2[1]:LINE_LENGTH} ")
-        sequence1 = sequence1[len(sequence1) - LINE_LENGTH:]
-        sequence2 = sequence2[len(sequence1) - LINE_LENGTH:]
+        print(f"{seq_1_name:IDENTIFIER_LENGTH}{seq_1_pos:SEQ_POSITION_LENGTH}{seq_1:LINE_LENGTH} ")
+        print(pad_string, middle_match_line(seq_1[:LINE_LENGTH], seq_2[:LINE_LENGTH], seq_1_pos, seq_2_pos))
+        print(f"{seq_2_name:IDENTIFIER_LENGTH}{seq_2_pos:SEQ_POSITION_LENGTH}{seq_2:LINE_LENGTH} ")
+        seq_1 = seq_1[len(seq_1) - LINE_LENGTH:]
+        seq_2 = seq_2[len(seq_1) - LINE_LENGTH:]
 
 def middle_match_line( sequence1, sequence2, sequence1_pos, sequence2_pos ):
     middle_line = ""
