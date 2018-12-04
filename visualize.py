c76b7714472121b5da8b8855a48535fa18cf45e1
Kimberlyn <kke2116@truman.edu>
2018-12-03 21:04:43 -0600

    added in pairwise visual

--- visualize.py
+++ visualize.py
@@ -1,49 +1,62 @@
 def alignment_output( alignment ):
-    seq_1 = alignment.aligned_sequences[0]
-    seq_2 = alignment.aligned_sequences[1]
+    seq1 = alignment.aligned_sequences[0]
+    seq2 = alignment.aligned_sequences[1]
 
     seq_1_name = alignment.name_list[0]
     seq_2_name = alignment.name_list[1]
+    
+    max_per_line = 50
 
-    IDENTIFIER_LENGTH = 16
-    LINE_LENGTH = 50
-    SEQ_POSITION_LENGTH = 4
+    dash_count_1 = 0
+    dash_count_2 = 0
+    
+    start_place = 0
+    end_place = max_per_line
+    place_mark = 0
+    while (len(seq1) // end_place) >= 1:
+        print(f'{seq_1_name:20} {(start_place + 1 - dash_count_1):3} {seq1[start_place : end_place]}')
 
-    longer_sequence = max(len(seq_1), len(seq_2))
-    seq_1_pos = 0
-    seq_2_pos = 0
-    pad_string = " " * (IDENTIFIER_LENGTH + SEQ_POSITION_LENGTH)
+        #create the middle part
+        alignment = ""
+        while place_mark < (end_place):
+            #print(place_mark)
+            if (seq1[place_mark] == '-'):
+                alignment += ' '
+                dash_count_1 += 1
+            elif (seq2[place_mark] == '-'):
+                alignment += ' '
+                dash_count_2 += 1
+            elif seq1[place_mark] == seq2[place_mark]:
+                alignment += '|'
+            else:
+                alignment += '.'
+            place_mark += 1
+        #print(alignment)
+        print((alignment).rjust(25 + max_per_line))
 
-    while longer_sequence > 0:
-        print(f"{seq_1_name:IDENTIFIER_LENGTH}{seq_1_pos:SEQ_POSITION_LENGTH}{seq_1:LINE_LENGTH} ")
-        print(pad_string, middle_match_line(seq_1[:LINE_LENGTH], seq_2[:LINE_LENGTH], seq_1_pos, seq_2_pos))
-        print(f"{seq_2_name:IDENTIFIER_LENGTH}{seq_2_pos:SEQ_POSITION_LENGTH}{seq_2:LINE_LENGTH} ")
-        seq_1 = seq_1[len(seq_1) - LINE_LENGTH:]
-        seq_2 = seq_2[len(seq_1) - LINE_LENGTH:]
+        print(f'{seq_2_name:20} {(start_place + 1 - dash_count_2):3} {seq2[start_place : end_place]}')
+        print()
+        
+        start_place += max_per_line
+        end_place += max_per_line
+        alignment = ""
 
-def middle_match_line( sequence1, sequence2, sequence1_pos, sequence2_pos ):
-    middle_line = ""
+    #print the leftovers if needed
+    if (len(seq1) % max_per_line) != 0:  
+        print(f'{seq_1_name:20} {(start_place + 1 - dash_count_1):3} {seq1[start_place:]}')
 
-    for i in range(len):
-        if sequence1[i] == '-' or sequence2[i] == '-':
-            middle_line += " "
-            if sequence1[i] == '-':
-                sequence1_pos += 1
-            if sequence2[i] == '-':
-                sequence2_pos += 1
-        elif sequence1[i] == sequence2[i]:
-            middle_line += "|"
-        else:
-            middle_line += "."
-
-    return middle_line
-
-def identical_site_percent( sequence1, sequence2 ):
-    total_sites = max(len(sequence1), len(sequence2))
-
-    identical_sites = 0
-    for i in range(total_sites):
-        if sequence1[i] == sequence2[i]:
-            identical_sites += 1
-
-    return identical_sites / total_sites
+        alignment = ""
+        for place_mark in range (start_place, len(seq1)):
+            if (seq1[place_mark] == '-'):
+                alignment += ' '
+                dash_count_1 += 1
+            elif (seq2[place_mark] == '-'):
+                alignment += ' '
+                dash_count_2 += 1
+            elif seq1[place_mark] == seq2[place_mark]:
+                alignment += '|'
+            else:
+                alignment += '.'
+        print((alignment).rjust(25 + len(seq1) - start_place))
+                
+        print(f'{seq_2_name:20} {(start_place + 1 - dash_count_2):3} {seq2[start_place:]}')
