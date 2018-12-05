54523e5da594f411a7c33727f97d850589e61322
Rian Markes <rmm5645@truman.edu>
2018-12-04 20:46:32 -0600

    Added multisequence output

--- visualize.py
+++ visualize.py
@@ -1,4 +1,5 @@
 def alignment_output( alignment ):
+  if( len(alignment.aligned_sequences) == 2 ):
     seq1 = alignment.aligned_sequences[0]
     seq2 = alignment.aligned_sequences[1]
 
@@ -38,6 +39,10 @@ def alignment_output( alignment ):
         #print(alignment)
         print((alignment).rjust(27 + max_per_line))
 
+    
+    #print( seq_1_name + " " + seq_1[0:50] )
+    #print( seq_2_name + " " + seq_2[0:50] )
+
         print(f'{seq_2_name:20} {(start_place + 1 - dash_count_2):5} {seq2[start_place : end_place]}')
         print()
         
@@ -64,3 +69,44 @@ def alignment_output( alignment ):
         print((alignment).rjust(27 + len(seq1) - start_place))
                 
         print(f'{seq_2_name:20} {(start_place + 1 - dash_count_2):5} {seq2[start_place:]}')
+
+  else:
+    sequences = alignment.aligned_sequences
+    sequences_name = alignment.name_list
+
+    for i in range( len(sequences_name) ):
+        sequences_name[i] = sequences_name[i][:19]
+
+    max_line = 50
+    start_printed = 0
+    start_index = 0
+    end = max_line
+    LONG_INDEX = 0 #The index of the longest string in the list if needed
+    dash_count = [0] * len(sequences) #List of the amount of dashes in each sequenes initialize to 0
+
+    print("Multisequence Alignment")
+  
+    for i in range ( len(sequences) ): #find index for longest string in list (only matters if alignments are different size)
+        if ( max( len(sequences[LONG_INDEX]), len(sequences[i]) ) == len(sequences[i]) ):
+          LONG_INDEX = i
+
+    while ( len(sequences[LONG_INDEX]) != 0 ): #print the sequences
+        print() #for spacing
+        for i in range ( len(sequences) ):
+          print(f'{sequences_name[i]:24} {(start_printed + 1 - dash_count[i]):4} {sequences[i][start_index : end]}')
+          for j in range ( len(sequences[i][start_index:end]) ):
+            if( sequences[i][j] == '-' ): #check to see it there's a gap
+                dash_count[i] += 1
+          sequences[i] = sequences[i][max_line:] #take off string that has been writen
+        start_printed += max_line
+
+
+def identical_site_percent( sequence1, sequence2 ):
+    total_sites = max(len(sequence1), len(sequence2))
+
+    identical_sites = 0
+    for i in range(total_sites):
+        if sequence1[i] == sequence2[i]:
+            identical_sites += 1
+
+    return identical_sites / total_sites
